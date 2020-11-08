780c1d850c2076713f6e76aaffb1fbf25fa51ae3
Joffre J Jatem <jjatem@hotmail.com>
2020-11-07 23:55:29 -0500

    fixing typos on cf provider script

--- terraform/cloudfare.tf
+++ terraform/cloudfare.tf
@@ -1,18 +1,20 @@
 provider "cloudflare" {
-  version = "~> 2.0"
-  token   = var.cloudflare_api_token
+  version   = "~> 2.0"
+  api_token = var.cloudflare_api_token
 }
 
 ## DNS ZONE
 
-data "cloudflare_zone" "cf_zones" {
-  filter = var.domain
+data "cloudflare_zones" "cf_zones" {
+  filter {
+    name = var.domain
+  }
 }
 
 ## DNS A RECORD
 # Add a record to the domain
 resource "cloudflare_record" "dns_record" {
-  zone_id = data.cloudfare_zones.cf_zones.zones[0].id
+  zone_id = data.cloudflare_zones.cf_zones.zones[0].id
   name    = "storybooks${terraform.workspace == "prod" ? "" : "-${terraform.workspace}"}"
   value   = google_compute_address.ip_address.address
   type    = "A"
